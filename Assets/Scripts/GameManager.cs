09e2c6e14e0279c5c063f3bf46564d003f987ee9
DuyPham169 <phamduy22062002@gmail.com>
2024-07-01 23:30:44 -0500

    7/1 Change

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Player player;
+
+    [Header("Fruits Management")]
+    public bool fruitsHaveRandomLook;
+    public int fruitsCollected;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddFruit() => fruitsCollected++;
+    public bool FruitsHaveRandomLook() => fruitsHaveRandomLook;
+}
