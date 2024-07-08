9230ccd731517089eb4253d6dcd7b849f1046d0e
DuyPham169 <phamduy22062002@gmail.com>
2024-07-07 22:46:11 -0500

    Game Manager + Mechanics

---
+++ Assets/Scripts/Player/PlayerAnimationEvent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationEvent : MonoBehaviour
+{
+    private Player player;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
+    public void FinishRespawn() => player.RespawnFinished(true);
+}
